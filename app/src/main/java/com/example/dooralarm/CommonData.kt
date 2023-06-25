95363b0cae7d7e7c91696a50264711e8c71180fa
srideep-banerjee <banerjee.srideep@gmail.com>
2023-06-25 23:41:26 +0530

    Added more code to handle fetch new logs from database and write them to file

--- app/src/main/java/com/example/dooralarm/CommonData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.dooralarm
-
-import android.content.Context
-
-class CommonData {
-    companion object{
-        public const val notificationChannelID = "dooralert_notif_id"
-    }
-}
--- app/src/main/java/com/example/dooralarm/MainActivity.kt
+++ app/src/main/java/com/example/dooralarm/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.example.dooralarm
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.graphics.BitmapFactory
-import android.os.Build
+import com.example.dooralarm.credentials.CommonData
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.main_activity.loadingScreen
-import kotlinx.android.synthetic.main.main_activity.recyclerView
-import kotlinx.android.synthetic.main.main_activity.switch1
+import com.example.dooralarm.databinding.MainActivityBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
@@ -25,14 +28,35 @@ import java.util.Locale
 class MainActivity : AppCompatActivity() {
 
     private val itemDataList = mutableListOf<ItemData>()
+    private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        try {
+            Class.forName("credentials.CommonData")
+        } catch (e: ClassNotFoundException) {
+            Toast.makeText(applicationContext, "Credentials file not found", Toast.LENGTH_LONG)
+                .show()
+            finish()
+            return
+        }
+
+        val databaseDeferred= lifecycleScope.async {Firebase.database(CommonData.databaseURL)}
+
+        val sensorStateFile = File("${filesDir}\\sensor state.txt")
+        if (sensorStateFile.exists()) {
+            val state = sensorStateFile.bufferedReader().use { it.readLine() }
+            binding.switch1.isChecked = state.equals("true")
+        } else sensorStateFile.createNewFile()
 
         val logsFile = File("${filesDir}/logs.txt")
         if (logsFile.exists()) {
+            binding.loadingTV.text = "Loading old logs"
+
             lifecycleScope.launch(Dispatchers.IO) {
                 logsFile.forEachLine {
                     val logData = it.split(",")
@@ -46,21 +70,93 @@ class MainActivity : AppCompatActivity() {
                         )
                     itemDataList.add(itemData)
                 }
+
                 withContext(Dispatchers.Main) {
-                    recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-                    recyclerView.adapter = CustomAdapter(itemDataList)
-                    loadingScreen.visibility = View.GONE
-                    recyclerView.visibility = View.VISIBLE
+                    binding.loadingTV.text = "Connecting to Server"
                 }
+
+                val database =databaseDeferred.await()
+
+                val myRef = database.reference
+                myRef.child("Obstacles Detected")
+                    .addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            val value = dataSnapshot.getValue(String::class.java)
+                            value?.let {
+                                if (it == "true") {
+                                    runOnUiThread {
+                                        binding.loadingTV.text = "Loading new logs"
+                                    }
+                                    getLogsFromDatabase(database)
+                                }
+                            }
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError) {
+                            runOnUiThread {
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Unable to fetch data",
+                                    Toast.LENGTH_LONG
+                                )
+                                    .show()
+                                finish()
+                            }
+                        }
+                    })
             }
         } else logsFile.createNewFile()
+        
+        lifecycleScope.launch { 
+            val database=databaseDeferred.await()
 
-        val sensorStateFile = File("${filesDir}\\sensor state.txt")
-        if (sensorStateFile.exists()) {
-            val state = sensorStateFile.bufferedReader().use { it.readLine() }
-            switch1.isChecked = state.equals("true")
-        } else sensorStateFile.createNewFile()
+        }
 
+    }
+
+    fun getLogsFromDatabase(database: FirebaseDatabase) {
+        val myRef = database.reference.child("/Logs")
+        myRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                lifecycleScope.launch(Dispatchers.IO) {
+                    var ind = 1
+                    val file = File(filesDir.path + "/logs.txt")
+                    if (!file.exists()) file.createNewFile()
+                    while (snapshot.hasChildren()) {
+                        val childSnap = snapshot.child("log ${ind++}")
+                        childSnap.getValue<String>()?.let {
+                            val logData = it.split(",")
+                            val deviceID = logData[0]
+                            val date = logData[1]
+                            val time = logData[2]
+                            val itemData =
+                                ItemData(
+                                    "Movement detected by device $deviceID",
+                                    "${getProcessedDate(date)},$time"
+                                )
+                            itemDataList.add(itemData)
+                            file.appendText("$it\n")
+                        }
+                        childSnap.ref.removeValue()
+                    }
+                    withContext(Dispatchers.Main) {
+                        binding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                        binding.recyclerView.adapter = CustomAdapter(itemDataList)
+                        binding.loadingScreen.visibility = View.GONE
+                        binding.recyclerView.visibility = View.VISIBLE
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                runOnUiThread {
+                    Toast.makeText(applicationContext, "Unable to fetch data", Toast.LENGTH_LONG)
+                        .show()
+                    finish()
+                }
+            }
+
+        })
     }
 
     override fun onDestroy() {
